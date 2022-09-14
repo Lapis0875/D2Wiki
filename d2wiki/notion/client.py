cae01d2dbee27f31c3e2b64eb018021a3bc2bed4
Lapis0875 <lapis0875@kakao.com>
2022-09-14 17:04:05 +0900

    feat: Now D2NotionWrapper.query_perk supports all perk rows (1~4)

--- d2wiki/notion/client.py
+++ d2wiki/notion/client.py
@@ -1,3 +1,4 @@
+from typing import ClassVar
 from uuid import UUID
 
 from notion_client import AsyncClient
@@ -5,6 +6,18 @@ from notion_client import AsyncClient
 from d2wiki.notion.models.rich_text import flatten_richtext
 from d2wiki.notion.models.weapon_perk import D2Perk
 from d2wiki.types import JSON
+
+
+class D2NotionRoute:
+    class Perks:
+        PerkRow1: ClassVar[str] = str(UUID("54a18fc21a45422da9567223463f512f"))
+        Barrel = Scope = Sight = PerkRow1  # alias
+        PerkRow2: ClassVar[str] = str(UUID("c45bc65fcdeb43738c31870481541f7d"))
+        Ammo = Magazine = PerkRow2  # alias
+        PerkRow34: ClassVar[str] = str(UUID("2a3d2c092ffb4c27be1fd3b6748a6ff1"))
+        Trait = PerkRow34  # alias
+
+    Wells: ClassVar[str] = str(UUID("feb9ab0ed69b4eccb8c4a16b303fd11c"))
 
 
 class D2NotionWrapper:
@@ -15,9 +28,9 @@ class D2NotionWrapper:
     def __init__(self, config: JSON):
         self.client: AsyncClient = AsyncClient(auth=config["token"])
 
-    async def query_perks(self, perk_name: str) -> list[D2Perk]:
+    async def query_perks(self, route: str, perk_name: str) -> list[D2Perk]:
         resp = await self.client.databases.query(**{
-            "database_id": str(UUID("c45bc65fcdeb43738c31870481541f7d")),
+            "database_id": route,
             "filter": {
                 "property": "이름",
                 "rich_text": {
@@ -27,9 +40,10 @@ class D2NotionWrapper:
         })
         from pprint import pprint
         pprint(resp)
+
         return [D2Perk.from_json(
             name=flatten_richtext(page["properties"]["이름"]["title"]),
             description=flatten_richtext(page["properties"]["설명"]["rich_text"]),
-            img_url=page["icon"]["file"]["url"]
+            img_url=page["icon"]["file"]["url"] if page.get("icon") is not None else None
         ) for page in resp["results"]]
 
